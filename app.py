fd190286ed909916c69be0ec8b642cd8668460f8
ItsKungFuPanda <academicsdm03@gmail.com>
2025-08-18 19:58:03 +0530

    Commit

--- app.py
+++ app.py
@@ -46,4 +46,53 @@ for i in range (0,100+1):
 a=1
 b=2
 print(a+b)
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+a=1
+b=2
+print(a+b)
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+a=1
+b=2
+print(a+b)
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+a=1
+b=2
+print(a+b)
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+for i in range (0,100+1):
+  for j in range (0,i*100+1):
+    print("*", end="")
+a=1
+b=2
+print(a+b)
+
 
